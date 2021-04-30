600c1eea3c56737237428f45cd3850661e870b4d
iericstudy <iericstudy@hotmail.com>
2021-04-29 21:25:34 -0400

    as

--- app/usuarios/edit.php
+++ app/usuarios/edit.php
@@ -16,16 +16,10 @@ $utilities = new Utilities();
 $service = new ServiceFile("usuarios");
 
 
-<<<<<<< HEAD
 if(isset($_POST["Nombre"]) && isset($_POST["Apellido"]) && isset($_POST["Partido_perteneceID"]) && isset($_POST["Partido_aspiraID"]) && isset($_FILES["Foto"]) && isset($_POST["Estado"]))
 {
   if(($_POST["Nombre"] != null) && ($_POST["Apellido"] != null) && ($_POST["Partido_perteneceID"] != null) && ($_POST["Partido_aspiraID"] != null) && ($_FILES["Foto"] != null))
-=======
-if(isset($_POST["Nombre"]) && isset($_POST["Apellido"]) && isset($_POST["Email"]) && isset($_POST["Usuario"]) && isset($_POST["Password"]))
->>>>>>> 0100bbaef0ccb6ccb63867b6efa31544df09b794
   {
-    if(($_POST["Nombre"] != null) && ($_POST["Apellido"] != null) && ($_POST["Email"] != null) && ($_POST["Usuario"] != null) && ($_POST["Password"] != null))
-    {
     
 
     $usuario = new usuario(
